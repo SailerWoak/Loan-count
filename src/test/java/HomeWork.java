293f5955504ef827bd3a074537430008821f5eab
artur <artur.gabetov@gmail.com>
2018-10-10 20:36:37 +0300

    my HomeWork

--- src/test/java/HomeWork.java
+++ src/test/java/HomeWork.java
@@ -2,18 +2,22 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class HomeWork {
+
         @Test
         public  void LoanTest() {
-
+            double firstTenYears, secondTenYears, thirdTenYears, sum, sum2,sum3;
             float total = 50000;
-            double firstTenYears = 0.1;
-            double secondTenYears = 0.08;
-            double thirdTenYears = 0.06;
-            double sum = total * firstTenYears;
-            double sum2 = total * secondTenYears;
-            double sum3 = total * thirdTenYears;
+            firstTenYears = 0.1;
+            secondTenYears = 0.08;
+            thirdTenYears = 0.06;
+             sum = total * firstTenYears;
+             sum2 = total * secondTenYears;
+             sum3 = total * thirdTenYears;
             double x = sum + sum2 + sum3;
-            System.out.println("He will over pay for 30 years: " + x );
+            System.out.println("Overpay with 10%: " + sum );
+            System.out.println("Overpay with 8%: " + sum2 );
+            System.out.println("Overpay with 6%: " + sum3 );
+            System.out.println("Total in 30 years: " + x );
               Assertions.assertEquals(12000,x,"wrong number ");
         }
 
